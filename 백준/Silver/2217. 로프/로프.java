313dda39a30775749acef4acac25dfec5e9c5788
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-04-24 15:38:57 +0900

    [Silver IV] Title: 로프, Time: 184 ms, Memory: 20716 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,23 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		ArrayList<Integer> list = new ArrayList<>();
+		int max = 0;
+		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
-			list.add(Integer.parseInt(br.readLine()));
+			arr[i] = Integer.parseInt(br.readLine());
+			max = max > arr[i] ? max : arr[i];
 		}
-
-		Collections.sort(list, Collections.reverseOrder());
+		
+		int[] nums = new int[max + 1];
+		for (int i = 0; i < n; i++) {
+			nums[arr[i]]++;
+		}
 		
 		int answer = 0;
-		for (int i = 0; i < n; i++) {
-			answer = answer < list.get(i) * (i + 1) ? list.get(i) * (i + 1) : answer;
+		for (int i = 1; i <= max; i++) {
+			if (nums[i] == 0) {
+				continue;
+			}
+			
+			int weight = i * n;
+			answer = answer > weight ? answer : weight;
+			n -= nums[i];
 		}
 		
 		System.out.println(answer);
