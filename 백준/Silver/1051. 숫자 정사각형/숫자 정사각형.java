c9bf5bbdb3409761258cd8db1f3dc7cf6a911cf0
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-05-09 17:25:27 +0900

    [Silver IV] Title: 숫자 정사각형, Time: 84 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] board = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < m; j++) {
+				board[i][j] = s.charAt(j) - '0';
+			}
+		}
+		
+		int answer = 1;
+		int min = n < m ? n : m;
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < m; col++) {
+				
+				for (int size = 1; size <= min; size++) {
+					if (col + size < m && row + size < n) 
+						if (board[row][col] == board[row][col + size]
+							&& board[row][col] == board[row + size][col]
+							&& board[row][col]== board[row + size][col + size])
+								answer = answer > size + 1 ? answer : size + 1;
+				}
+			}
+		}
+		
+		System.out.println(answer * answer);
+	}
+}
