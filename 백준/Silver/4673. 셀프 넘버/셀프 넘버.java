a51e939054c8c30c6c9c0839b72e7c31693f4ad6
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-06-02 23:45:33 +0900

    [Silver V] Title: 셀프 넘버, Time: 76 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,25 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Main {
+    static int end = 10_000;
     public static void main(String[] args) {
-        int end = 10_000;
         boolean[] nums = new boolean[end + 1];
         for (int i = 1; i < nums.length; i++) {
-            int num = i;
-            char[] arr = String.valueOf(num).toCharArray();
-            for (int j = 0; j < arr.length; j++) {
-                num += arr[j] - '0';
-            }
-            if (num <= end) {
-                nums[num] = true;
-            }
+            nums[check(i)] = true;
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i < nums.length; i++) {
-            if (!nums[i]) {
-                System.out.println(i);
-            }
+            if (!nums[i]) sb.append(i + "\n");
         }
+        sb.delete(sb.length() - 1, sb.length());
+        System.out.println(sb);
+    }
+
+    private static int check(int num) {
+        int sum = num;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        if (sum <= end) return sum;
+        else return 0;
     }
 }
