2683edde6498ea3bc13e12bb9ebc296e71511c5e
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-06-14 01:33:08 +0900

    [Silver II] Title: 에디터, Time: 432 ms, Memory: 85008 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,48 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int n = Integer.parseInt(br.readLine());
-        Stack<Character> left = new Stack<>();
-        Stack<Character> right = new Stack<>();
+        LinkedList<Character> list = new LinkedList<>();
 
         for (int i = 0; i < s.length(); i++) {
-            left.push(s.charAt(i));
+            list.add(s.charAt(i));
+        }
+        // listIterator : 자체적으로 커서가 있다.
+        // 초기 생성 시 커서가 0으로 만들어짐
+        // 커서 : 요소 사이에 있음
+        // next() : 커서 위치를 오른쪽으로 옮기고 커서의 왼쪽 요소를 반환함
+        // previous() : 커서 위치를 왼쪽으로 옮기고 커서의 오른쪽 요소를 반환함
+        // remove() : 커서의 오른쪽 요소를 제거함
+        ListIterator<Character> iter = list.listIterator();
+
+        // 조건 : 명령어가 수행되기 전에 커서는 문장의 맨 뒤에 위치하고 있다고 한다.
+        while (iter.hasNext()) {
+            iter.next();
         }
 
         for(int i = 0; i < n; i++) {
             String command = br.readLine();
             switch (command.charAt(0)) {
                 case 'L':
-                    if (!left.isEmpty())
-                        right.push(left.pop());
+                    if (iter.hasPrevious()) {
+                        iter.previous();
+                    }
                     break;
                 case 'D':
-                    if (!right.isEmpty())
-                        left.push(right.pop());
+                    if (iter.hasNext()) {
+                        iter.next();
+                    }
                     break;
                 case 'B':
-                    if (!left.isEmpty())
-                        left.pop();
+                    if (iter.hasPrevious()) {
+                        iter.previous();
+                        iter.remove();
+                    }
                     break;
                 case 'P':
-                    left.push(command.charAt(2));
+                    iter.add(command.charAt(2));
                     break;
             }
         }
 
-        while (!left.isEmpty()) {
-            right.push(left.pop());
-        }
-
+        // LinkedList는 for문보다 for-each를 사용하는게 더 빠르다!
         StringBuilder sb = new StringBuilder();
-        while(!right.isEmpty()) {
-            sb.append(right.pop());
+        for (Character x : list) {
+            sb.append(x);
         }
         System.out.println(sb);
     }
