163ac8055ba22f7932209b0b74c74c47c762cece
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-06-14 02:45:57 +0900

    [Silver III] Title: 수열, Time: 200 ms, Memory: 21816 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int answer = sum;
+        for (int i = 0; i < n - k; i++) {
+            sum += arr[i + k] - arr[i];
+            answer = answer > sum ? answer : sum;
+        }
+        System.out.println(answer);
+    }
+}
