5bc54291eef75726663438f282a03b49b9eb00a4
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-06-16 11:52:17 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,57 +4,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-    static int[] nums;
-    static int[] operators;
+    static int n = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+    static int[] nums, op;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         nums = new int[n];
-        operators = new int[4];
+        op = new int[4];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operators[i] = Integer.parseInt(st.nextToken());
+            op[i] = Integer.parseInt(st.nextToken());
         }
 
-        recursion(1, nums[0]);
+        recursion(1, nums[0], 0, 0, 0, 0);
         System.out.println(max);
         System.out.println(min);
     }
 
-    private static void recursion(int depth, int sum) {
+    private static void recursion(int depth, int sum, int plus, int sub, int multi, int div) {
         if (depth == n) {
             max = max > sum ? max : sum;
             min = min > sum ? sum : min;
             return;
         }
 
-        if (operators[0] > 0) {
-            operators[0]--;
-            recursion(depth + 1, sum + nums[depth]);
-            operators[0]++;
+        if (plus < op[0]) {
+            recursion(depth + 1, sum + nums[depth], plus + 1, sub, multi, div);
         }
 
-        if (operators[1] > 0) {
-            operators[1]--;
-            recursion(depth + 1, sum - nums[depth]);
-            operators[1]++;
+        if (sub < op[1]) {
+            recursion(depth + 1, sum - nums[depth], plus, sub + 1, multi, div);
         }
 
-        if (operators[2] > 0) {
-            operators[2]--;
-            recursion(depth + 1, sum * nums[depth]);
-            operators[2]++;
+        if (multi < op[2]) {
+            recursion(depth + 1, sum * nums[depth], plus, sub, multi + 1, div);
         }
 
-        if (operators[3] > 0) {
-            operators[3]--;
-            recursion(depth + 1, sum / nums[depth]);
-            operators[3]++;
+        if (div < op[3]) {
+            recursion(depth + 1, sum / nums[depth], plus, sub, multi, div + 1);
         }
     }
 
