352b95b0eff679fdb55efb9e73c2e1d9164c9379
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-06-23 17:28:13 +0900

    [Silver III] Title: 수리공 항승, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -1,19 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int l = Integer.parseInt(input[1]);
 
-        st = new StringTokenizer(br.readLine());
         boolean[] counts = new boolean[1001];
+        input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            counts[(Integer.parseInt(st.nextToken()))] = true;
+            counts[Integer.parseInt(input[i])] = true;
         }
 
         int answer = 0;
