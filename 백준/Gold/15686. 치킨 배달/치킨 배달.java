1480d7524c987e49a25acb226009ea6cdb3572bd
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-06-27 02:27:54 +0900

    [Gold V] Title: 치킨 배달, Time: 196 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,36 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class House {
+    static class Point {
         int row;
         int col;
 
-        public House(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
-
-    static class Chicken{
-        int row;
-        int col;
-        int dist;
-
-        public Chicken(int row, int col) {
+        public Point(int row, int col) {
             this.row = row;
             this.col = col;
         }
     }
 
     static int n, m;
-    static int min = Integer.MAX_VALUE;
-    static ArrayList<House> hList = new ArrayList<>();
-    static ArrayList<Chicken> cList = new ArrayList<>();
-    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
+    static ArrayList<Point> house = new ArrayList<>();
+    static ArrayList<Point> chicken = new ArrayList<>();
+    static boolean[] selected;
     static int[][] matrix;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,62 +33,45 @@ public class Main {
                 int num = Integer.parseInt(st.nextToken());
                 matrix[i][j] = num;
                 if (num == 1) {
-                    hList.add(new House(i, j));
+                    house.add(new Point(i, j));
                 } else if (num == 2) {
-                    cList.add(new Chicken(i, j));
+                    chicken.add(new Point(i, j));
                 }
             }
         }
 
-        visited = new boolean[cList.size()];
+        selected = new boolean[chicken.size()];
 
-        for (int i = 1; i <= m; i++) {
-            recursion(0, 0, i);
-        };
+        recursion(0, 0);
 
-        System.out.println(min);
+        System.out.println(answer);
     }
 
-    public static void recursion(int depth, int count, int max) {
-        if (depth <= cList.size() && count == max) {
-            min = Math.min(min, getChickenDistance());
-            return;
-        }
+    public static void recursion(int index, int count) {
+        // 치킨집이 많을 수록 치킨 거리는 작아질 확률이 높음
+        // 따라서 1부터 m까지가 아닌, m일때만을 계산
+        if (count == m) {
+            int result = 0;
 
-        if (depth == cList.size()) {
-            return;
-        }
-
-        recursion(depth + 1, count, max);
-        visited[depth] = true;
-        recursion(depth + 1, count + 1, max);
-        visited[depth] = false;
-    }
-
-    public static int getChickenDistance(){
-        int answer = 0;
-
-        // 집에서 가장 가까운 치킨집 구하자
-        for (int i = 0; i < hList.size(); i++) {
-            House home = hList.get(i);
-
-            int sum = 0;
-            int dist =  1;
-            out:while (true) {
-                for (int j = 0; j < cList.size(); j++) {
-                    if (visited[j]) {
-                        if (Math.abs(cList.get(j).row - home.row)
-                            + Math.abs(cList.get(j).col - home.col) == dist) {
-                            sum += dist;
-                            break out;
-                        }
+            for (int i = 0; i < house.size(); i++) {
+                int tmp = Integer.MAX_VALUE;
+                for (int j = 0; j < chicken.size(); j++) {
+                    if (selected[j]) {
+                        int dist = Math.abs(house.get(i).row - chicken.get(j).row)
+                                + Math.abs(house.get(i).col - chicken.get(j).col);
+                        tmp = Math.min(tmp, dist);
                     }
                 }
-                dist++;
+                result += tmp;
             }
-            answer += sum;
+            answer = Math.min(result, answer);
+            return;
         }
 
-        return answer;
+        for (int i = index; i < chicken.size(); i++) {
+            selected[i] = true;
+            recursion(i + 1, count + 1);
+            selected[i] = false;
+        }
     }
 }
