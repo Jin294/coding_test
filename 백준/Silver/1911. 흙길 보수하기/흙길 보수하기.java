22754a5418c13d709ceab1125708bf17ed8707c6
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-07-02 23:35:13 +0900

    [Silver I] Title: 흙길 보수하기, Time: 292 ms, Memory: 17964 KB -BaekjoonHub

--- 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.java
@@ -1,41 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class Board implements Comparable<Board>{
+        int start;
+        int end;
+
+        public Board(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Board o) {
+            if (this.start == o.start) {
+                return this.end - o.end;
+            }
+            return this.start - o.start;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
-        int[][] matrix = new int[n][2];
+        PriorityQueue<Board> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            matrix[i][0] = Integer.parseInt(st.nextToken());
-            matrix[i][1] = Integer.parseInt(st.nextToken());
+            pq.add(new Board(
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())));
         }
 
-        Arrays.sort(matrix, ((o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        }));
-
-        int answer = 0;
-        int range = 0;
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] > range) {
-                range  = matrix[i][0];
+        int answer = 0; // 널빤지 개수
+        int range = 0; // 널빨지로 물 웅덩이를 덮고있는 범위
+        while (!pq.isEmpty()) {
+            Board target = pq.poll();
+            if (target.start > range) {
+                range  = target.start;
             }
 
-            if (matrix[i][1] > range) {
-                while (matrix[i][1] > range) {
-                    range += l;
-                    answer++;
-                }
+            while (target.end > range) {
+                range += l;
+                answer++;
             }
         }
 
