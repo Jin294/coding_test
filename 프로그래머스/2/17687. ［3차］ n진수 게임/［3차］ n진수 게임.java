246c3c23470ffbc5ad57392a03afd1627fb7967b
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-11-27 21:08:38 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.84 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int t, int m, int p) {
+        // 1. 전체 수를 n진법으로 변환
         StringBuilder sb = new StringBuilder();
         for (int i = 0; sb.length() <= m * t; i++) {
             sb.append(Integer.toString(i, n));
@@ -10,6 +11,7 @@ class Solution {
         String num = sb.toString();
         StringBuilder answer = new StringBuilder();
         
+        // 2. m명중에 p번째
         int idx = 0;
         int len = num.length();
         while (answer.length() < t) {
