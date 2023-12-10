bbab0328bd3e021197d2ab4c3953c18e634008e8
Jin294 <60572470+Jin294@users.noreply.github.com>
2023-12-11 00:31:57 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.38 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int x : arr) {
+            pq.add(x);
+        }
+        
+        while (pq.size() != 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            
+            pq.add(LCM(a, b));
+        }
+        
+        return pq.poll();
+    }
+    
+    public int LCM(int a, int b) {
+        int gcm = GCM(a, b);
+        return gcm == 0 ? 0 : (a / gcm) * b;
+    }
+    
+    public int GCM(int a, int b) {
+        return b == 0 ? a : GCM(b, a % b);
+    }
+}
