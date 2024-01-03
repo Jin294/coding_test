670e931026ac2361b20b5fd46b51f17cdbd6db2b
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-01-03 10:39:10 +0900

    [level 3] Title: 양과 늑대, Time: 0.21 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -4,44 +4,44 @@ class Solution {
     static int[] gInfo;
     static int[][] gEdges;
     
-    static int answer;
+    static boolean[] visited;
+    
     static final int SHEEP = 0;
-    static final int WOLF = 1;
+    static int answer;
     public int solution(int[] info, int[][] edges) {
         gInfo = info;
         gEdges = edges;
         
-        DFS(0, 0, 0, new boolean[info.length]);
+        visited = new boolean[info.length];
+        
+        DFS(0, 0, 0);
         
         return answer;
     }
     
-    public void DFS(int now, int sheepCnt, int wolfCnt, boolean[] isVisited) {
+    public void DFS(int now, int sheepCnt, int wolfCnt) {
         if (gInfo[now] == SHEEP) {
             sheepCnt++;
         } else {
             wolfCnt++;
         }
-        
+                
         if (sheepCnt <= wolfCnt) {
             return;
         }
-                
-        isVisited[now] = true;
         answer = Math.max(answer, sheepCnt);
         
+        visited[now] = true;
+        
         for (int[] edge : gEdges) {
-            int parent = edge[0];
-            int child = edge[1];
+            int p = edge[0];
+            int c = edge[1];
             
-            if (isVisited[parent] && !isVisited[child]) {
-                boolean[] nextVisited = new boolean[gInfo.length];
-                for (int i = 0; i < nextVisited.length; i++) {
-                    nextVisited[i] = isVisited[i];
-                }
-                
-                DFS(child, sheepCnt, wolfCnt, nextVisited);
+            if (visited[p] && !visited[c]) {
+                DFS(c, sheepCnt, wolfCnt);
             }
         }
+        
+        visited[now] = false;
     }
 }
