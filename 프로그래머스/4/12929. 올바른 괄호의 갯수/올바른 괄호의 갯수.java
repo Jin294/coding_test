3bf0bb262085f547e3d69bc6b62418a210abd2b2
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-01-11 11:47:40 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,16 +1,49 @@
+import java.util.*;
+
 class Solution {
+    static Set<String> set;
     public int solution(int n) {
-        int answer = 0;
+        set = new HashSet<>();
+        int open = n;
+        int close = n;
         
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
+        makeBrace(open, close, new String(""));
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                dp[i] += dp[(i - 1) - j] * dp[j];
+        return set.size();
+    }
+    
+    public void makeBrace(int open, int close, String now) {
+        if (open == 0 && close == 0) {
+            isCorrect(now);
+            return;
+        }
+        
+        if (open != 0) {
+            makeBrace(open - 1, close, now + "(");
+        }
+        if (close != 0) {
+            makeBrace(open, close - 1, now + ")");
+        }
+    }
+    
+    public void isCorrect(String now) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = now.toCharArray();
+        for (char x : arr) {
+            if (x == '(') {
+                stack.add(x);
+            } else {
+                if (stack.isEmpty()) {
+                    return;
+                }
+                stack.pop();
             }
         }
         
-        return dp[n];
+        if (!stack.isEmpty()) {
+            return;
+        }
+        
+        set.add(now);
     }
 }
