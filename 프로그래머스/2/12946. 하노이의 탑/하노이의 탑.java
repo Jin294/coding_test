a61ad2fe472c46cc1463ffd6fe1510eeb285835e
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-01-17 11:38:52 +0900

    [level 2] Title: 하노이의 탑, Time: 3.87 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,25 +1,19 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<int[]> list;
+    static ArrayList<int[]> moves;
     public int[][] solution(int n) {
-        list = new ArrayList<>();
-        recursion(n, 1, 3, 2);
-        int[][] answer = new int[list.size()][2];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+        moves = new ArrayList<>();
+        makeHanoiTower(n, 1, 3, 2);
+        
+        return moves.toArray(new int[moves.size()][2]);
     }
     
-    public void recursion(int n, int start, int end, int mid) {
-        if (n == 1) {
-            list.add(new int[]{start, end});
-            return;
+    public void makeHanoiTower(int n, int start, int end, int mid) {
+        if (n > 0) {
+            makeHanoiTower(n - 1, start, mid, end);
+            moves.add(new int[]{start, end});
+            makeHanoiTower(n - 1, mid, end, start);
         }
-        
-        recursion(n - 1, start, mid, end);
-        recursion(1, start, end, mid);
-        recursion(n - 1, mid, end, start);
     }
 }
