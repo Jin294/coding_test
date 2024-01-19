58697a0c21448ed527c866e1fa581506e8a5ea9b
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-01-19 14:32:14 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 36.47 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,6 +1,62 @@
+import java.util.*;
+
 class Solution {
+    static int len;
+    static long max = 0l;
+    static int[] gSequence, plus, minus;
+    static long[] dp1, dp2;
     public long solution(int[] sequence) {
-        long answer = 0;
-        return answer;
+        gSequence = sequence;
+        len = sequence.length;
+        
+        // 배열 길이만큼 펄스 수열 만듦
+        plus = makePulse(1, -1);
+        minus = makePulse(-1, 1);
+        
+        // dp 만들기
+        dp1 = makeArrayDynamic(sequence, plus);
+        dp2 = makeArrayDynamic(sequence, minus);
+        
+        return Math.max(getMax(dp1), getMax(dp2));
+    }
+    
+    public long getMax(long[] dp) {
+        // 최대인 인덱스 찾기
+        int idx = 0;
+        long max = dp[idx];
+        for (int i = 0; i < len; i++) {
+            if (max < dp[i]) {
+                max = dp[i];
+                idx = i;
+            }
+        }
+        
+        for (int i = 0; i < idx; i++) {
+            max = Math.max(max, dp[idx] - dp[i]);
+        }
+        
+        System.out.println(max);
+        return max;
+    }
+    
+    public long[] makeArrayDynamic(int[] sequence, int[] arr) {
+        long[] dp = new long[len];
+        
+        dp[0] = sequence[0] * arr[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] += (long) dp[i - 1] + (long) (sequence[i] * arr[i]);
+        }
+        
+        return dp;
+    }
+    
+    public int[] makePulse(int first, int second) {
+        int[] arr = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+            arr[i] = (i % 2 == 0) ? first : second;
+        }
+        
+        return arr;
     }
 }
