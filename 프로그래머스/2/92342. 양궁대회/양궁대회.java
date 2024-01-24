066099dc7651aefe99799e6dacbcbcfc402edd30
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-01-24 23:02:58 +0900

    [level 2] Title: 양궁대회, Time: 0.84 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -2,81 +2,72 @@ import  java.util.*;
 
 class Solution {
     static final int LEN = 11;
-    static int diff;
-    static int[] answer, apeach, ryan;
+    static int maxDiff;
+    static int[] answer = {-1};
     public int[] solution(int n, int[] info) {
-        // 이길 수 없는 경우의 리턴배열
-        int[] lose = {-1};
+        int[] ryan = new int[LEN];
+        DFS(0, n, info, ryan);
 
-        // 라이언 점수배열 전역화
-        apeach = info;
-        ryan = new int[LEN];
-        answer = new int[LEN];
-
-        DFS(0, n, 0, 0);
-
-        return diff == 0 ? lose : answer;
+        return answer;
     }
     
-    // idx : 인덱스, 10 - idx 번째 점수를 얻기 위한 화살개수
-    // arrow : 잔여화살 개수
-    // aSum : 어피치의 점수합계
-    // rSum : 라이언의 점수합계
-    public void DFS(int idx, int arrow, int aSum, int rSum) {
-        // 화살이 다 떨어졌을 때
-        if (arrow < 0) {
-            return;
-        }
-
-        // 인덱스 끝까지 도달했을 때
-        // 또는 준비한 화살이 떨어졌을 때
-        if (idx == LEN) {
-            // 라이언의 점수가 어피치보다 높고
-            // 이전까지의 최댓값을 경신할 수 있을 때
-            if (aSum < rSum && diff <= rSum - aSum) {
-                // 아직 화살이 남아있다면 -> 0점에 몰아주자
-                if (arrow > 0) {
-                    ryan[LEN - 1] += arrow;
-                }
-
-                if (diff == rSum - aSum) {
-                    answer = getLowestArray(answer, ryan);
-                } else {
-                    diff = rSum - aSum;
-                    answer = Arrays.copyOf(ryan, LEN);
-                }
-            }
-            return;
-        }
-
-
-        // 해당 점수를 얻기 위한 화살개수
-        // 어피치보다 많이 맞혀야 점수획득이 가능하다.
-        int need2Gain = apeach[idx] + 1;
-        ryan[idx] = need2Gain;
-
-        // (10 - idx) 의 점수를 얻을 수 있을 때,
-        int score = 10 - idx;
-        DFS(idx + 1, arrow - need2Gain, aSum, rSum + score);
-
-        // (10 - idx) 의 점수를 얻을 수 없을 때,
-        ryan[idx] = 0;
-        DFS(idx + 1, arrow, apeach[idx] == 0 ? aSum : aSum + score, rSum);
-    }
-    
-    public int[] getLowestArray(int[] a, int[] b) {
-        long aString = scoringReverse(a);
-        long bString = scoringReverse(b);
-
-        return aString > bString ? Arrays.copyOf(a, LEN) : Arrays.copyOf(b, LEN);
-    }
-
-    public long scoringReverse(int[] arr) {
-        StringBuilder sb = new StringBuilder();
+    public boolean isRyanBetter(int[] ryan) {
         for (int i = LEN - 1; i >= 0; i--) {
-            sb.append(arr[i]);
+            if (answer[i] > ryan[i]) {
+                return false;
+            } else if (answer[i] < ryan[i]){
+                return true;
+            }
         }
 
-        return Long.parseLong(sb.toString());
+        return false;
+    }
+
+    public void calcScore(int[] apeach, int[] ryan) {
+        int apeachScore = 0, ryanScore = 0;
+        for (int i = 0; i < LEN; i++) {
+            int score = 10 - i;
+            if (apeach[i] > ryan[i]) {
+                apeachScore += score;
+            } else if (apeach[i] < ryan[i]){
+                ryanScore += score;
+            }
+        }
+
+        int diff = ryanScore - apeachScore;
+
+        // 현재 차이값이 최대 차이값보다 같다면
+        // answer 배열이 가장 낮은 점수를 고려했는지 살펴봐야 한다.
+        if (diff > 0 && diff >= maxDiff) {
+            if (maxDiff == diff && !isRyanBetter(ryan)) {
+                return;
+            }
+            maxDiff = diff;
+            answer = Arrays.copyOf(ryan, LEN);
+        }
+    }
+
+    public void DFS(int idx, int arrows, int[] apeach, int[] ryan) {
+        // 인덱스 끝까지 도달했을 때, 또는 화살이 다 떨어졌을 때
+        if (idx == LEN || arrows == 0) {
+            // 남은 화살은 모두 0점 과녁에 몰아준다
+            ryan[LEN - 1] += arrows;
+            calcScore(apeach, ryan);
+
+            // 0점에 몰아준 화살 복구
+            ryan[LEN - 1] -= arrows;
+            return;
+        }
+
+        // 해당 영역에 화살을 쏘자
+        if (apeach[idx] < arrows) {
+            // 어피치보다 많아야 해당 구역의 점수를 얻을 수 있음.
+            ryan[idx] += apeach[idx] + 1;
+            DFS(idx + 1, arrows - (apeach[idx] + 1), apeach, ryan);
+            ryan[idx] -= apeach[idx] + 1;
+        }
+
+        // 해당 영역에 화살을 쏘지 말자
+        DFS(idx + 1, arrows, apeach, ryan);
     }
 }
