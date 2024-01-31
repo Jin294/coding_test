60c40214edbd6634d28347793ae93461c40cdd37
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-01-31 12:02:33 +0900

    [level 3] Title: 부대복귀, Time: 62.71 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class Solution {
+    static final int MAX = Integer.MAX_VALUE;
     static ArrayList<ArrayList<Integer>> graph;
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         // 편의상, 정점개수 V 와 간선 개수 E를 저장
@@ -10,9 +11,7 @@ class Solution {
         // 최단시간(거리)를 저장할 배열
         int[] dist = new int[V + 1];
         // 최단시간(거리)를 경신하기 위해 최댓값으로 초기화
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        // 출발지점은 이동 필요 없으므로 0으로 설정
-        dist[destination] = 0;
+        Arrays.fill(dist, MAX);
         
         // 그래프 만들기
         graph = new ArrayList<>();
@@ -27,32 +26,28 @@ class Solution {
             graph.get(end).add(start);
         }
         
-        // 방문여부 체크할 부울배열
-        boolean[] visited = new boolean[V + 1];
-        
-        // 너비우선탐색과정을 메서드로 분리
-        BFS(visited, dist, destination);
+        dijkstra(dist, destination);
 
         int[] answer = new int[sources.length];
         for (int i = 0; i < sources.length; i++) {
-            answer[i] = dist[sources[i]] != Integer.MAX_VALUE ? dist[sources[i]] : -1;
+            answer[i] = dist[sources[i]] != MAX ? dist[sources[i]] : -1;
         }
         return answer;
     }
     
-    public void BFS(boolean[] visited, int[] dist, int now) {
+    public void dijkstra(int[] dist, int destination) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(now);
-        visited[now] = true;
+        q.add(destination);
+        dist[destination] = 0;
+        
         while(!q.isEmpty()) {
             int start = q.poll();
             
             ArrayList<Integer> list = graph.get(start);
             for (int i = 0; i < list.size(); i++) {
                 int arrival = list.get(i);
-                if (!visited[arrival]) {
-                    visited[arrival] = true;
-                    dist[arrival] = Math.min(dist[start] + 1, dist[arrival]);
+                if (dist[arrival] > dist[start] + 1) {
+                    dist[arrival] = dist[start] + 1;
                     q.add(arrival);
                 }
             }
