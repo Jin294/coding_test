5a50701017832514d6e2a9c247ae7cab1fab43ad
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-02-23 08:24:03 +0900

    [Silver II] Title: 주식, Time: 940 ms, Memory: 308452 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,30 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= t; tc++) {
+        for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
             int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
             }
-
-            long answer = 0;
-            int max = arr[n - 1];
-            for (int i = n - 2; i > -1; i--) {
-                if (arr[i] > max) {
-                    max = arr[i];
-                } else if (arr[i] < max) {
-                    answer += max - arr[i];
-                }
-            }
-            System.out.println(answer);
+            System.out.println(solution(n, arr));
         }
+    }
+
+    public static long solution(int n, int[] arr) {
+        long answer = 0;
+
+        int max = arr[n - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] >= max) {
+                max = arr[i];
+            } else {
+                answer += max - arr[i];
+            }
+        }
+
+        return answer;
     }
 }
