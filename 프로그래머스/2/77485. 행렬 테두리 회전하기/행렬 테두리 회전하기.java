e4b382efc567642540625c8873f060363a4cc66d
Jin294 <60572470+Jin294@users.noreply.github.com>
2024-02-26 00:27:57 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 4.72 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -12,51 +12,46 @@ class Solution {
         
         int[] answer = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            answer[i] = getMinRotateClockwise(queries[i]);
+            answer[i] = rotateClockwiseAndGetMin(queries[i]);
         }
         
         return answer;
     }
     
-    public int getMinRotateClockwise(int[] query) {
-        int r1 = query[0] - 1;
-        int c1 = query[1] - 1;
-        int r2 = query[2] - 1;
-        int c2 = query[3] - 1;
+    public int rotateClockwiseAndGetMin(int[] query) {
+        int startRow = query[0] - 1;
+        int startCol = query[1] - 1;
+        int endRow = query[2] - 1;
+        int endCol = query[3] - 1;
         
-        int min = matrix[r1][c1];
-        int save = matrix[r1][c1]; // 오리지널 r1, c1의 값
+        int minValue = matrix[startRow][startCol];
+        int originalValue = matrix[startRow][startCol]; 
         
-        // 시계방향이면, 거꾸로 반시계방향으로 거슬러 올라가야 한다.
-        // 왼쪽이 위로 올라온다 => r1, c1부터 내려가야 한다.
-        for (int i = r1; i < r2; i++) {
-            min = Math.min(matrix[i][c1], min);
-            matrix[i][c1] = matrix[i + 1][c1];
+        for (int i = startRow; i < endRow; i++) {
+            minValue = Math.min(matrix[i][startCol], minValue);
+            matrix[i][startCol] = matrix[i + 1][startCol];
         }
         
-        // 바닥이 왼쪽으로 이동한다. => r2, c1부터 오른쪽으로
-        for (int i = c1; i < c2; i++) {
-            min = Math.min(matrix[r2][i], min);
-            matrix[r2][i] = matrix[r2][i + 1];
+        for (int i = startCol; i < endCol; i++) {
+            minValue = Math.min(matrix[endRow][i], minValue);
+            matrix[endRow][i] = matrix[endRow][i + 1];
         }
                 
-        // 오른쪽이 아래로 내려간다 => r2, c2부터 올라간다
-        for (int i = r2; i > r1; i--) {
-            min = Math.min(matrix[i][c2], min);
-            matrix[i][c2] = matrix[i - 1][c2];
+        for (int i = endRow; i > startRow; i--) {
+            minValue = Math.min(matrix[i][endCol], minValue);
+            matrix[i][endCol] = matrix[i - 1][endCol];
         }
         
-        int tmp = matrix[r1][c1]; // 왼쪽이 위로 올라오면서, 오리지널의 한 칸 아래값이 저장됨
-        matrix[r1][c1] = save;
+        int lastValue = matrix[startRow][startCol];
+        matrix[startRow][startCol] = originalValue;
         
-        // 천장이 오른쪽으로 이동한다 => r1, c2부터 왼쪽으로
-        for (int i = c2; i > c1; i--) {
-            min = Math.min(matrix[r1][i], min);
-            matrix[r1][i] = matrix[r1][i - 1];
+        for (int i = endCol; i > startCol; i--) {
+            minValue = Math.min(matrix[startRow][i], minValue);
+            matrix[startRow][i] = matrix[startRow][i - 1];
         }
         
-        matrix[r1][c1] = tmp;
+        matrix[startRow][startCol] = lastValue;
                 
-        return min;
+        return minValue;
     }
 }
