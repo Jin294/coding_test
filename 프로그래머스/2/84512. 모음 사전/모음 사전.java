00998b59de6b45bc5a93f429cd24ab670f0bed49
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-03-12 23:18:40 +0900

    [level 2] Title: 모음 사전, Time: 12.25 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 class Solution {
     Set<String> set = new HashSet<>();
-    int[] nums = {0, 4, (int)('I' - 'A'), (int)('O' - 'A'), (int)('U' - 'A')};
+    char[] vowels = {'A', 'E', 'I', 'O', 'U'};
     public int solution(String word) {
         makeWords(new StringBuilder(), 0);
         
         List<String> list = new ArrayList<>(set);
         Collections.sort(list);
-        return list.indexOf(word);
+        return list.indexOf(word) + 1;
     }
     
     public void makeWords(StringBuilder sb, int depth) {
-        set.add(sb.toString());
         if (depth == 5) {
             return;
         }
         
         for (int i = 0; i < 5; i++) {
-            sb.append((char)('A' + nums[i]));
+            sb.append(vowels[i]);
+            set.add(sb.toString());
             makeWords(sb, depth + 1);
             sb.setLength(sb.length() - 1);
         }
