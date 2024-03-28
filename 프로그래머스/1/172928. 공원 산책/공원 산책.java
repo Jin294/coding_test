a6b8faa17a4f9be05b6792ef8a87c4f9208236ee
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-03-28 11:58:19 +0900

    [level 1] Title: 공원 산책, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -8,9 +8,11 @@ class Solution {
     
     Map<Character, Integer> directions = new HashMap<>();
     public int[] solution(String[] park, String[] routes) {
+    	// 가로 세로를 변수에 저장
         h = park.length;
         w = park[0].length();
         
+        // 방향별 인덱스 연결
         directions.put('N', 0);
         directions.put('S', 1);
         directions.put('W', 2);
@@ -19,25 +21,21 @@ class Solution {
         int[] answer = getStartPoint(park);
         
         for (String route : routes) {
-            answer = move(park, route, answer[0], answer[1]);
+            answer = move(park, route, answer);
         }
        
         return answer;
     }
     
-    public boolean isObstacle(String[] park, int row, int col) {
-        return park[row].charAt(col) == 'X';
-    }
-    
-    public int[] move(String[] park, String route, int row, int col) {
-        int[] now = {row, col};
-        
+    // 이동
+    public int[] move(String[] park, String route, int[] now) {
         int direction = directions.get(route.charAt(0));
         int distance = (int) (route.charAt(2) - '0');
         
-        int nr = row;
-        int nc = col;
+        int nr = now[0];
+        int nc = now[1];
         
+        // 한 칸씩 이동해야 경로상에 장애물이 있는지를 알 수 있다!
         for (int i = 0; i < distance; i++) {
             nr += dy[direction];
             nc += dx[direction];
@@ -51,14 +49,20 @@ class Solution {
             }
         }
         
-                
         return new int[]{nr, nc};
     }
     
+    // 범위를 벗어났는지 검사
     public boolean isWithinRange(int row, int col) {
         return row >= 0 && col >= 0 && row < h && col < w;
     }
     
+    // 현재 칸이 장애물인지 검사
+    public boolean isObstacle(String[] park, int row, int col) {
+        return park[row].charAt(col) == 'X';
+    }
+        
+    // 시작위치 S 찾기
     public int[] getStartPoint(String[] park) {
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
