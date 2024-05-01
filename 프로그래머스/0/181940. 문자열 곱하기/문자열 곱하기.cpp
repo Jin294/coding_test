364aa41ea26658e9e8813d2e3313044318c1f7fc
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-02 00:20:09 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cpp
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cpp
@@ -1,12 +1,9 @@
 #include <string>
-#include <vector>
 
 using namespace std;
 
 string solution(string my_string, int k) {
     string answer = "";
-    while (k-- > 0) {
-        answer += my_string;
-    }
+    while (k-- > 0) answer += my_string;
     return answer;
 }
