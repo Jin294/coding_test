35090b71585652807200cb5f226cf69b2721cd71
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-04 23:32:54 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.09 ms, Memory: 5.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<int, string> mapp = {{1, "w"}, {-1, "s"}, {10, "d"}, {-10, "a"}};
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    
+    for (int i = 1; i < numLog.size(); i++) {
+        int diff = numLog[i] - numLog[i - 1];
+        
+        answer += mapp[diff];
+    }
+    
+    return answer;
+}
