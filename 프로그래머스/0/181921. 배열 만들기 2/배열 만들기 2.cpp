697947e367d500457a1a36efda054698535d4c82
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-05 01:23:38 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cpp
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cpp
@@ -1,37 +1,31 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-void recursion(int len, int now, string str, vector<int>* list) {
-    if (len == now) {
-        (*list).push_back(atoi(str.c_str()));
-        return;
+void generateNumber(int start, int end, int num, vector<int>& answer) {
+    if (num >= start && num <= end) {
+        answer.push_back(num);
     }
     
-    recursion(len, now + 1, str + "5", list);
-    recursion(len, now + 1, str + "0", list);
+    if (num > end) return;
+    
+    generateNumber(start, end, num * 10, answer);
+    generateNumber(start, end, num * 10 + 5, answer);
 }
 
 vector<int> solution(int l, int r) {
-    vector<int> list = {0, 5};
-    
-    for (int i = 2; i <= 7; i++) {
-        recursion(i, 1, "5", &list);
-    }
-    
-    sort(list.begin(), list.end());
-    
     vector<int> answer;
-    for (int i = 0; i < list.size(); i++) {
-        if (list[i] >= l && list[i] <= r) {
-            answer.push_back(list[i]);
-        }
-    }
     
-    if (answer.size() == 0) {
+    if (l == 0) answer.push_back(l);
+    
+    generateNumber(l, r, 5, answer);
+        
+    sort(answer.begin(), answer.end());
+    
+    if (answer.empty()) {
         answer.push_back(-1);
     }
+    
     return answer;
 }
