a8d8ab0910138f7e70d87570ab197dc70686181a
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-11 23:47:28 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.21 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -2,16 +2,7 @@
 
 using namespace std;
 
-void upper(string& str) {
-    for (auto& x : str) {
-        x = toupper(x);
-    }
-}
-
 int solution(string myString, string pat) {
-    // 대문자로 통일
-    upper(myString);
-    upper(pat);
-    if (myString.find(pat) != string::npos) return 1;
+    if (toupper(myString).find(toupper(pat)) != string::npos) return 1;
     return 0;
 }
