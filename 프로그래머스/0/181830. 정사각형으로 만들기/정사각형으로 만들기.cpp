cf03710e3e605eed01bb5b179af6df390f582dcb
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-17 23:35:48 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 2.19 ms, Memory: 6.38 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.cpp
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.cpp
@@ -7,13 +7,12 @@ vector<vector<int>> solution(vector<vector<int>> arr) {
     int colSize = arr[0].size();
     
     if (rowSize > colSize) { // 세로가 더 길다면 => 가로길이를 맞춰야
-        for (auto& vc : arr) {
-            for (int i = 0; i < rowSize - colSize; i++) vc.push_back(0);
+        for (auto& row : arr) {
+            row.resize(rowSize, 0);
         }
     } else { // 가로가 더 길다면 => 세로길이를 맞춰야
-        vector<int> tmp;
-        for (int i = 0; i < colSize; i++) tmp.push_back(0);
-        for (int i = 0; i < colSize - rowSize; i++) arr.push_back(tmp);
+        vector<int> tmp (colSize, 0);
+        arr.resize(colSize, tmp);
     }
     
     return arr;
