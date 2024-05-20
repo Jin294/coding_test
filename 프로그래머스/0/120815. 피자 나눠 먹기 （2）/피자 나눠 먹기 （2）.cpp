78ed26bd314341720cbf12762730890c28ef0559
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-21 00:31:33 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -10,7 +10,6 @@ int calculateGCD(int first, int second) {
 int solution(int n) {
     int first = max(6, n);
     int second = min(6, n);
-    int gcd = calculateGCD(first, second);
     
-    return first / gcd * second / 6;
+    return n / calculateGCD(first, second) ;
 }
