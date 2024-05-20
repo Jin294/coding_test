7742908d3fe939e98869cea188513f22fafea6d8
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-21 08:53:51 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int solution(vector<int> array) {
-    vector<int> count(1000);
-    fill(count.begin(), count.end(), 0);
-    sort(array.begin(), array.end());
+    vector<int> count(1000, 0); // 생성 및 0으로 초기화
     for (int num : array) count[num]++;
     
     // 최빈값 == 최고로 많이 나온 키
     int key = -1;
     
     // 최빈값의 개수
+    // max_element는 iterator를 반환하기 때문에 *를 붙여야 값을 가져올 수 있음
     int val = *max_element(count.begin(), count.end());
     
     // 최빈값이 둘 이상인지 확인
@@ -21,10 +20,11 @@ int solution(vector<int> array) {
         if (count[i] == val) {
             key = i;
             cnt++;
+            
+            // 최빈값이 여러개라면 -1을 리턴
+            if (cnt > 1) return -1;
         }
     }
     
-    // 최빈값이 여러개라면 -1을 리턴
-    if (cnt > 1) return -1;
     return key;
 }
