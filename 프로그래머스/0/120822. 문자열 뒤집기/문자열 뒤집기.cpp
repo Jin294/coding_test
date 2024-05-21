f84b1de37f37db79abbf52faf2c0811eb4bd6f4f
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-22 00:12:10 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    reverse(my_string.begin(), my_string.end());
+    return my_string;
+    
+}
