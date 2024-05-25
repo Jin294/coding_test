2ea847e14cac14762539c6f3d611fa2cb9a0c50b
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-26 00:18:36 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> box, int n) {
+    int answer = 1;
+    
+    for (int num : box) {
+        answer *= num / n;
+    }
+    
+    return answer;
+}
