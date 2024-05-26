2e634af74f0eee148a46967d92a6071f80fb161a
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-27 00:42:30 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -4,11 +4,6 @@ using namespace std;
 
 string solution(string my_string) {
     map<char, int> mapp;
-    for (int i = 0; i < 26; i++) {
-        mapp['a' + i] = 0;
-    }
-    mapp[' '] = 0;
-    
     string answer = "";
     for (const char& ch : my_string) {
         if (mapp[ch] == 0) {
