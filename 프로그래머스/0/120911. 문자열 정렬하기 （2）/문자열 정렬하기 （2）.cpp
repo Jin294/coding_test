f4e9c179eeabcd3311b4390d67fad127ecaae892
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-29 00:57:31 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string) {
+    for (char &ch : my_string) ch = tolower(ch);
+    sort(my_string.begin(), my_string.end());
+    return my_string;
+}
