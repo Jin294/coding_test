68b7ceb6d38f3e159efb4c430d66281c8301b0c1
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-05-28 21:52:42 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -1,15 +1,9 @@
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 int solution(vector<string> s1, vector<string> s2) {
-    map<string, int> mapp;
-    for (string s : s1) mapp[s]++;
-    
     int answer = 0;
-    for (string s : s2) if (mapp[s] > 0) answer++;
-    
     return answer;
 }
