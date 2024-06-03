98c16882d216ee837ebf87bd8b1c1cafb2f9f71a
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-03 12:58:19 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.cpp
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int solution(vector<int> common) {
-    int last = common[common.size() - 1];
     if (common[1] - common[0] == common[2] - common[1]) {
         // 등차
-        return last + common[1] - common[0];
+        return common.back() + common[1] - common[0];
     } else {
         // 등비
-        return last * (common[1] / common[0]);
+        return common.back() * (common[1] / common[0]);
     }
 }
