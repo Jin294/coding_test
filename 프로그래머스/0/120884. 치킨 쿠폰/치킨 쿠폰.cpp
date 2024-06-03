ad3303e2febea0d12c8522833af278618e2e4816
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-03 12:53:58 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -1,15 +1,14 @@
 using namespace std;
 
 int solution(int chicken) {
-    int coupon = chicken;
     int service = 0;
     
-    int tmp = coupon / 10;
+    int tmp = chicken / 10;
     while (tmp > 0) {
-        coupon %= 10;
+        chicken %= 10;
         service += tmp;
-        coupon += tmp;
-        tmp = coupon / 10;
+        chicken += tmp;
+        tmp = chicken / 10;
     }
     
     return service;
