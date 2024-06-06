40d3931d8c4eb28ce4b8686e68bd72577fcf144d
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-06 21:02:30 +0900

    [level 2] Title: 방문 길이, Time: 0.12 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -2,14 +2,18 @@
 
 using namespace std;
 
+// 주어진 범위를 벗어나는지 검사
 bool checkBoundary(int row, int col) {
     return row >= -5 && row <= 5 && col >= -5 && col <= 5;
 }
 
 int solution(string dirs) {
     // 글자에 따른 방향을 매핑하는 directions
-    unordered_map<char, pair<int, int>> directions{
-        {'U', {-1, 0}}, {'D', {1, 0}}, {'R', {0, 1}}, {'L', {0, -1}}
+    unordered_map<char, pair<int, int>> directions {
+        {'U', {-1, 0}},
+        {'D', {1, 0}},
+        {'R', {0, 1}},
+        {'L', {0, -1}}
     };
     
     int row = 0;
@@ -20,14 +24,17 @@ int solution(string dirs) {
 
     for (char dir : dirs) {
         auto [dr, dc] = directions[dir];
+        
+        // 일단 이동 우선
         int nr = row + dr;
         int nc = col + dc;
         
         if (checkBoundary(nr, nc)) {
-            // 양방향 경로를 모두 기록
+            // 이동한 값이 범위 안에 속한다면 양방향 경로를 모두 기록
             records.insert({{row, col}, {nr, nc}});
             records.insert({{nr, nc}, {row, col}});
             
+            // 이동한 값으로 업데이트
             row = nr;
             col = nc;
         }
