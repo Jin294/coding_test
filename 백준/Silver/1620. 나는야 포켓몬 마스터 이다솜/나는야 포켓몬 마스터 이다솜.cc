ebdeb9ef41b2510ac6f820bea14d8ba8a9cf89e6
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-13 08:52:18 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 156 ms, Memory: 23528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,24 +1,33 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int n ,m;
-string s;
-map<string, int> names;
-map<int, string> indexes;
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
-  cin >> n >> m;
-  for (int i = 1; i <= n; i++) {
-    cin >> s;
-    names[s] = i;
-    indexes[i] = s;
-  }
-  for (int i = 0; i < m; i++) {
-    cin >> s;
-    if (atoi(s.c_str()) == 0) { // 문자가 들어올 때
-      cout << names[s] << "\n";
-    } else { // 숫자가 들어올 때
-      cout << indexes[atoi(s.c_str())] << "\n";
-    }
-  }
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, m, tmp;
+	string token;
+	cin >> n >> m;
+	unordered_map<int, string> numToStr;
+	unordered_map<string, int> strToNum;
+	
+	for (int i = 1; i <= n; i++) {
+		cin >> token;
+		numToStr.insert({i, token});
+		strToNum.insert({token, i});
+	}
+
+	for (int i = 0; i < m; i++) {
+		cin >> token;
+		
+		if (isdigit(token[0])) {
+			tmp = stoi(token);
+			cout << numToStr[tmp] << "\n";
+		} else {
+			cout << strToNum[token] << "\n";
+		}
+	}
+	
+	return 0;
 }
