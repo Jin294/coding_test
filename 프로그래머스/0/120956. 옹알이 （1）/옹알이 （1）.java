288a31091ee616d13f181099716d09be69dc7832
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-16 22:38:32 +0900

    [level 0] Title: 옹알이 (1), Time: 0.13 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,27 +2,20 @@ class Solution {
     String[] spells = {"aya", "ye", "woo", "ma"};
     
     public int solution(String[] babbling) {
+        int answer = 0;
+        
         // 1. 주어진 옹알이에 네 가지 발음이 포함되어 있는지 확인 후, 대체
+        // 각 문자열에 발음은 최대 한 번만 들어있다.
         for (int i = 0; i < babbling.length; i++) {
             for (String spell : spells) {
-                babbling[i] = babbling[i].replaceAll(spell, "1");
+                babbling[i] = babbling[i].replace(spell, "1");
             }
-        }
-
-        // 2. 옹알이 단어가 순수 알파벳으로 이루어져 있다면 ok
-        int answer = 0;
-        for (String b : babbling) {
-            if (isAllAlphabethic(b)) answer++;
+            babbling[i] = babbling[i].replace("1", "");
+            
+            // 2. 옹알이 단어가 비어있다면 카운트
+            if (babbling[i].isEmpty()) answer++;
         }
 
         return answer;
-    }
-    
-    public boolean isAllAlphabethic(String str) {
-        for (char x : str.toCharArray()) {
-            if (!Character.isDigit(x)) return false;
-        }
-        
-        return true;
     }
 }
