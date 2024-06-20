ebc6689c18d81b0dc520a7a1a483280494d08715
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-21 01:27:55 +0900

    [Silver II] Title: 유기농 배추, Time: 80 ms, Memory: 13336 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -4,61 +4,65 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m, k;
-	static int[] dy = {0, 1, 0, -1};
-	static int[] dx = {1, 0, -1, 0};
-	static int[][] board;
-	static boolean[][] visited;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		
-		for (int tc = 0; tc < t; tc++) {
-			st = new StringTokenizer(br.readLine());
-			m = Integer.parseInt(st.nextToken());
-			n = Integer.parseInt(st.nextToken());
-			k = Integer.parseInt(st.nextToken());
-			
-			board = new int[n][m];
-			visited = new boolean[n][m];
-			for (int i = 0; i < k; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
-				board[b][a] = 1;
-			}
-			
-			int count = 0;
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (!visited[i][j] && board[i][j] == 1) {
-						DFS(i, j);
-						count++;
-					}
-				}
-			}
-			
-			System.out.println(count);
-		}
-	}
+    // 좌표 이동을 위한 배열
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
 
-	private static void DFS(int row, int col) {
-		
-		visited[row][col] = true;
-		
-		for (int i = 0; i < 4; i++) {
-			int ny = row + dy[i];
-			int nx = col + dx[i];
-			
-			if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
-				continue;
-			}
-			
-			if (!visited[ny][nx] && board[ny][nx] == 1) {
-				DFS(ny, nx);
-			}
-		}
-	}
+    static boolean[][] matrix, visited;
+    static int n, m;
+
+    public static boolean isRangeWithin(int row, int col) {
+        return row >= 0 && row < n && col >= 0 && col < m;
+    }
+
+    public static void DFS(int row, int col) {
+        for (int i = 0; i < 4; i++) {
+            int nr = row + dy[i];
+            int nc = col + dx[i];
+
+            // 범위 안에 해당되는가
+            if (isRangeWithin(nr, nc)) {
+                if (matrix[nr][nc] &&!visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    DFS(nr, nc);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            // 데이터 입력
+            int t = Integer.parseInt(br.readLine());
+            while (t-- > 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                m = Integer.parseInt(st.nextToken());
+                n = Integer.parseInt(st.nextToken());
+                int k = Integer.parseInt(st.nextToken());
+                matrix = new boolean[n][m];
+                for (int i = 0; i < k; i++) {
+                    st = new StringTokenizer(br.readLine());
+                    int col = Integer.parseInt(st.nextToken());
+                    int row = Integer.parseInt(st.nextToken());
+
+                    matrix[row][col] = true;
+                }
+
+                // 방문처리를 위한 부울배열 : 같은 곳 재방문안하게 체크
+                visited = new boolean[n][m];
+
+                int count = 0;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        if (matrix[i][j] && !visited[i][j]) {
+                            visited[i][j] = true;
+                            DFS(i, j);
+                            count++;
+                        }
+                    }
+                }
+                System.out.println(count);
+            }
+        }
+    }
 }
