7fc0396fda12d7cfb43eefffc9fc5396f00a41f1
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-20 22:29:50 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12240 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,75 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Node{
-    int y;
-    int x;
-    int depth;
+class Point {
+    int row;
+    int col;
+    int cnt;
 
-    public Node(int y, int x, int depth) {
-        this.y = y;
-        this.x = x;
-        this.depth = depth;
+    public Point(int row, int col, int cnt) {
+        this.row = row;
+        this.col = col;
+        this.cnt = cnt;
     }
 }
 
 public class Main {
-    static int[][] board;
-    static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-
-    public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 데이터 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        visited = new boolean[n][m];
-
+        int[][] matrix = new int[n][m];
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < m; j++) {
-                if (str.charAt(j) == '1') {
-                    board[i][j] = 1;
-                }
+                matrix[i][j] = str.charAt(j) - '0';
             }
         }
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, 1));
-        visited[0][0] = true;
-        ArrayList<Integer> list = new ArrayList<>();
+        // 좌표 이동을 위한 배열
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
+
+        // 방문처리를 위한 부울배열 : 같은 곳 재방문안하게 체크
+        boolean[][] visited = new boolean[n][m];
         
-        // 메인 알고리즘
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            int row = cur.y;
-            int col = cur.x;
+        // BFS : 넓이우선탐색
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0, 0, 1));
 
-            // 목적지에 도달했을 때
-            if (row == n - 1 && col == m - 1) {
-                list.add(cur.depth);
-                continue;
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            
+            // 목적지에 다다르면 프로그램 종료
+            if (cur.row == n - 1 && cur.col == m - 1) {
+                System.out.println(cur.cnt);
+                return;
             }
 
-            // 상하좌우 갈 수 있는 칸을 큐에 넣기
-            for (int i = 0; i < 4; i++) {
-                int x = col + dx[i];
-                int y = row + dy[i];
+            visited[cur.row][cur.col] = true;
 
-                // 이동할 수 있는 칸 && 방문한 적 없으면 큐에 넣는다.
-                if ((y > -1 && y < n && x > -1 && x < m) && board[y][x] == 1&& !visited[y][x]) {
-                    visited[y][x] = true;
-                    queue.add(new Node(y, x, cur.depth + 1));
+            for (int i = 0; i < 4; i++) {
+                int nr = cur.row + dy[i];
+                int nc = cur.col + dx[i];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    if (!visited[nr][nc] && matrix[nr][nc] != 0) {
+                        visited[nr][nc] = true;
+                        q.add(new Point(nr, nc, cur.cnt + 1));
+                    }
                 }
             }
         }
 
-        // 여러 경로로 도달 가능하다면 최솟값을 취한다
-        Collections.sort(list);
-        System.out.println(list.get(0));
     }
 }
