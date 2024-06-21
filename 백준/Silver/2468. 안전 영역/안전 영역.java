7d6840a1d6453044875f7982f58dd798eaed8f9d
Jinho Lee <60572470+Jin294@users.noreply.github.com>
2024-06-21 23:08:47 +0900

    [Silver I] Title: 안전 영역, Time: 192 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,14 +7,31 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n;
-    public static boolean isRangeWithin(int y, int x) {
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[][] matrix;
+    static boolean[][] visited;
+
+    public static boolean isInRange(int y, int x) {
         return y >= 0 && x >= 0 && y < n && x < n;
+    }
+
+    public static void DFS(int y, int x, int curLevel) {
+        visited[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (isInRange(ny, nx) && !visited[ny][nx] && matrix[ny][nx] > curLevel) {
+                DFS(ny, nx, curLevel);
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
         int min = 100;
         int max = 1;
-        int[][] matrix;
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             // 데이터 입력
             n = Integer.parseInt(br.readLine());
@@ -25,23 +41,20 @@ public class Main {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < n; j++) {
                     matrix[i][j] = Integer.parseInt(st.nextToken());
+
+                    // 최소, 최대높이 업데이트
                     min = Math.min(min, matrix[i][j]);
                     max = Math.max(max, matrix[i][j]);
                 }
             }
         };
 
+        // 모든 지역이 물에 잠기는 것을 감안해서 최솟값을 1로 설정함.
         int answer = 1;
 
         // 좌표 이동을 위한 배열
-        int[] dy = {-1, 1, 0, 0};
-        int[] dx = {0, 0, -1, 1};
-
-        Queue<int[]> q;
-        boolean[][] visited;
         for (int level = min; level <= max; level++) {
             int count = 0;
-            q = new LinkedList<>();
             visited = new boolean[n][n];
 
             // 이차원 배열 탐색
@@ -49,27 +62,8 @@ public class Main {
                 for (int j = 0; j < n; j++) {
                     // 물에 잠기지 않았으면서 방문하지 않은 곳
                     if (matrix[i][j] > level && !visited[i][j]) {
-                        visited[i][j] = true;
-                        q.add(new int[]{i, j});
-
-                        // 현제 높이에서 침수되지 않은 곳의 개수 세는 변수
+                        DFS(i, j, level);
                         count++;
-
-                        // BFS로 level 이하 높이인 지역의 개수 세기
-                        while (!q.isEmpty()) {
-                            int[] now = q.poll();
-
-                            for (int k = 0; k < 4; k++) {
-                                int ny = now[0] + dy[k];
-                                int nx = now[1] + dx[k];
-
-                                // 1. 범위체크, 2. 방문여부 체크, 3. 침수여부 체크
-                                if (isRangeWithin(ny, nx) && !visited[ny][nx] && matrix[ny][nx] > level) {
-                                    visited[ny][nx] = true;
-                                    q.add(new int[]{ny, nx});
-                                }
-                            }
-                        }
                     }
                 }
             }
